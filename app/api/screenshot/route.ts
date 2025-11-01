13469018eea68bf9b3175c0b5d59ac5fa5843b7e
redplug <blies@naver.com>
2025-11-02 00:07:04 +0900

    update

--- app/api/screenshot/route.ts
+++ app/api/screenshot/route.ts
@@ -90,7 +90,7 @@ export async function GET(request: Request) {
     const KOREAN_BUTTON_SELECTOR = 'div.settings.svelte-ghcjle > div > div > select > option:nth-child(4)'; // 예: '#app > header > button.lang-ko'
 
     const startSelector = '.svelte-1oecyh1';
-    const endSelector = '#app > div.main.en.svelte-1oecyh1 > div.ads-widget-container.svelte-1oecyh1';
+    const endSelector = '.svelte-1oecyh1';
 
     try {
       // 1. [추가] 끝 요소가 렌더링될 때까지 최대 5초간 기다립니다.
